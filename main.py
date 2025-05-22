3cc2fdd3385d205c8d8f8f0005f25c41e0a234bf
srishtidesai026 <srishtipdesai01@gmail.com>
2025-05-22 09:59:28 +0530

    face authentication done

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import os
 import eel
 from engine.features import *
 from engine.commands import *
+from engine.auth import recognize
 
 def start():
     
     eel.init("www")
 
     playAssistantSound()
+    flag = recognize.AuthenticateFace()
     
     os.system('start msedge.exe --app="http://localhost:8000/index.html"')
     
