84cbdc08da385c53fca84f39fc121fb2594501cb
srishtidesai026 <srishtipdesai01@gmail.com>
2025-06-11 20:40:16 +0530

    breakCommit

--- main.py
+++ main.py
@@ -7,9 +7,24 @@ from engine.auth import recognize
 def start():
     
     eel.init("www")
-
     playAssistantSound()
-    flag = recognize.AuthenticateFace()
+
+    @eel.expose
+    def init():
+
+        eel.hideLoader()
+        speak("Ready for Face Authentication...")
+
+        flag = recognize.AuthenticateFace()
+        if(flag==1):
+            eel.hideFaceAuth()
+            speak("Face Authentication Successful!")
+            eel.hideFaceAuthSuccess()
+            speak("Hi Srishti! I'm Mai, your desktop assistant, how can I help you today?")
+            playAssistantSound()
+            eel.hideStart()
+        else:
+            speak("Face Authentication Failed!")
     
     os.system('start msedge.exe --app="http://localhost:8000/index.html"')
     
--- www/controller.js
+++ www/controller.js
@@ -3,11 +3,37 @@ $(document).ready(function () {
     // Expose the function globally
     eel.expose(displayMsg);
 
+
+
     function displayMsg(msg) {
-        console.log("displayMsg called with:", msg);  // Debug check
-        $(".sirimsg p").html(msg.replace(/\n/g, "<br>").replace(/\*/g, "•"));
-        $(".sirimsg").textillate('start');
+        console.log("displayMsg called with:", msg);
+        const element = $(".siri-message");
+        // Stop any running animation
+        element.textillate('stop');
+        // Set new content
+        element.html(msg.replace(/\n/g, "<br>").replace(/\*/g, "•"));
+        // Reinitialize textillate
+        element.textillate({
+            in: {
+                effect: 'fadeInUp',
+                delayScale: 1.5,
+                delay: 50,
+                sync: false
+            },
+            out: {
+                effect: 'fadeOut',
+                sync: false
+            },
+            loop: false,
+            autoStart: true
+        });
+        // Start animation
+        element.textillate('start');
     }
+    // console.log("displayMsg called with:", msg);  // Debug check
+    // $(".sirimsg p").html(msg.replace(/\n/g, "<br>").replace(/\*/g, "•"));
+    // $(".sirimsg").textillate('start');
+
 
     eel.expose(showHood);
     function showHood() {
@@ -49,6 +75,54 @@ $(document).ready(function () {
 
             chatBox.scrollTop = chatBox.scrollHeight;
         }
+    }
+
+
+    // Hide Loader and display Face Auth animation
+    eel.expose(hideLoader)
+    function hideLoader() {
+
+        $("#Loader").attr("hidden", true);
+        $("#FaceAuth").attr("hidden", false);
+
+    }
+
+    // Hide Face auth and display Face Auth success animation
+    eel.expose(hideFaceAuth)
+    function hideFaceAuth() {
+
+        $("#FaceAuth").attr("hidden", true);
+        $("#FaceAuthSuccess").attr("hidden", false);
+
+    }
+
+    // Hide success and display 
+    eel.expose(hideFaceAuthSuccess)
+    function hideFaceAuthSuccess() {
+
+        $("#FaceAuthSuccess").attr("hidden", true);
+        $("#HelloGreet").attr("hidden", false);
+
+    }
+
+
+    // Hide Start Page and display blob
+    eel.expose(hideStart)
+    function hideStart() {
+
+        $("#Start").attr("hidden", true);
+
+        setTimeout(function () {
+            $("#oval").addClass("animate__animated animate__zoomIn");
+
+        }
+
+            , 1000)
+        setTimeout(function () {
+            $("#oval").attr("hidden", false);
+        }
+
+            , 1000)
     }
 });
 
